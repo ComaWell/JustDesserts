7902ff417ed231efd34b027c7be471819fcd91a3
Connor Wellington <connor.matthew.wellington@gmail.com>
2021-07-08 21:33:38 -0400

    Update JDK Build Version to 12

--- src/me/connor/util/Nullable.java
+++ src/me/connor/util/Nullable.java
@@ -8,7 +8,6 @@ import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.MODULE;
 import static java.lang.annotation.ElementType.PACKAGE;
 import static java.lang.annotation.ElementType.PARAMETER;
-import static java.lang.annotation.ElementType.RECORD_COMPONENT;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.ElementType.TYPE_PARAMETER;
 import static java.lang.annotation.ElementType.TYPE_USE;
